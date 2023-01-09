dcd39fd35b04391d2500a518d28a31ed514d8253
zchunkai <97151329+zchunkai@users.noreply.github.com>
2023-01-09 17:21:05 +0800

    声明配置类

---
+++ WebSocketConfig.java
@@ -0,0 +1,13 @@
+package cn.platform.thinglinks.monitor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
